ca7be4827298fbf334f727f0d37ee00397dac827
Junebeomstics <kjb961013@gmail.com>
2021-07-14 18:37:30 +0900

    baseline code(xgboost) by Junbeom

--- preproc_baseline.py
+++ preproc_baseline.py
@@ -1,6 +1,6 @@
 import numpy as np
 def covariance_matrx(data):
-    n_regions = 22
+    n_regions = 48
     A = np.zeros((n_regions, n_regions))
     for i in range(n_regions):
         for j in range(i, n_regions):
@@ -9,27 +9,26 @@ def covariance_matrx(data):
             else:
                 A[i][j] = abs(np.corrcoef(data[i, :], data[j, :])[0][1])  # get value from corrcoef matrix
                 A[j][i] = A[i][j]
-    upper_tri_flattened = A[np.triu_indices(22, k=0)]
+    upper_tri_flattened = A[np.triu_indices(48, k=0)]
     #print(upper_tri_flattened)
     return upper_tri_flattened
 
-
+def gaussian_matrix(data):
+    n_regions = 48
 
 
 if __name__ == '__main__':
-    train_data = np.load('data/train_data_1200_1.npy').squeeze()
-    test_data = np.load('data/test_data_1200_1.npy').squeeze()
-    train_label = np.load('data/train_label_1200_1.npy')
-    test_label = np.load('data/test_label_1200_1.npy')
-    train_data_covar = np.zeros((train_data.shape[0], 253))
-    test_data_covar = np.zeros((test_data.shape[0], 253))
+    train_data = np.load('/scratch/bigdata/ABCD/abcd-fmriprep-rs/ST-GCN-data/harvardoxford/harvardoxford_train_data_1.npy').squeeze()
+    test_data = np.load('/scratch/bigdata/ABCD/abcd-fmriprep-rs/ST-GCN-data/harvardoxford/harvardoxford_test_data_1.npy').squeeze()
+    train_label = np.load('/scratch/bigdata/ABCD/abcd-fmriprep-rs/ST-GCN-data/harvardoxford/harvardoxford_train_label_1.npy')
+    test_label = np.load('/scratch/bigdata/ABCD/abcd-fmriprep-rs/ST-GCN-data/harvardoxford/harvardoxford_test_label_1.npy')
+    train_data_covar = np.zeros((train_data.shape[0], 1176)) #batch * 
+    test_data_covar = np.zeros((test_data.shape[0], 1176))
     for i in range(train_data.shape[0]):
         train_data_covar[i] = covariance_matrx(train_data[i].T)
         i += 1
-
-
     for i in range(test_data.shape[0]):
         test_data_covar[i] = covariance_matrx(test_data[i].T)
         i += 1
-    np.save('data/train_data_1200_1_mlp.npy', train_data_covar)
-    np.save('data/test_data_1200_1_mlp.npy', test_data_covar)
+    np.save('./data/train_data_baseline_1.npy', train_data_covar)
+    np.save('./data/test_data_baseline_1.npy', test_data_covar)
---
+++ run_baseline.py
@@ -0,0 +1,120 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import h2o
+from h2o.estimators import H2OXGBoostEstimator
+import xgboost as xgb
+from catboost import CatBoostClassifier, Pool
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class mlp(nn.Module):
+    def __init__(self, input_dim):
+        super(mlp, self).__init__()
+        self.input_dim = input_dim
+        self.linear1 = nn.Linear(self.input_dim, 64)
+        self.linear2 = nn.Linear(64, 1)
+
+    def forward(self, x):
+        linear1_out = self.linear1(x)
+        linear2_out =  self.linear2(linear1_out)
+        output = torch.sigmoid(linear2_out)
+        return output
+
+def train(train_data, test_data, train_label, test_label):
+    train_data = torch.from_numpy(train_data).float().to(device)
+    test_data = torch.from_numpy(test_data).float().to(device)
+    train_label = torch.from_numpy(train_label).float().to(device)
+    test_label = torch.from_numpy(test_label).float().to(device)
+    print(train_label)
+    model = mlp(train_data.shape[1])
+    loss = nn.BCELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    n_epochs = 20000
+    epoch_list = []
+    acc_list = []
+    loss_list = []
+    with open('output/mlp_baseline/training_output.txt', 'w') as f:
+        f.truncate(0)
+
+    with open('output/mlp_baseline/testing_output.txt', 'w') as f:
+        f.truncate(0)
+    for epoch in range(n_epochs):
+        model.train()
+        model.zero_grad()
+        pred_probs = model(train_data)
+        y_pred = pred_probs > 0.5
+        loss_val = loss(pred_probs, train_label)
+        loss_val.backward()
+        optimizer.step()
+        acc = accuracy_score(train_label, y_pred)
+        print("Epoch: {}, Loss: {}, Accuracy: {}".format(epoch, loss_val, acc))
+        with open('output/mlp_baseline/training_output.txt', 'a') as f:
+            f.write("Epoch: {} Loss: {} Accuracy: {}\n".format(epoch, loss_val, acc))
+        if epoch % 100 == 0:
+            print("-"*80)
+            print("Testing Epoch: {}".format(epoch))
+            model.eval()
+            with torch.no_grad():
+                pred_prob_test = model(test_data)
+                y_pred_test = pred_prob_test > 0.5
+                loss_val_test = loss(pred_prob_test, test_label)
+                acc_test = accuracy_score(test_label, y_pred_test)
+
+                print("Epoch: {}, Loss: {}, Accuracy: {}".format(epoch, loss_val_test, acc_test))
+            with open('output/mlp_baseline/testing_output.txt', 'a') as f:
+                f.write("Epoch: {} Loss: {} Accuracy: {}\n".format(epoch, loss_val_test, acc_test))
+            model.train()
+            print("-"*80)
+        #if epoch
+
+def train_xgboost(train_data, test_data, train_label, test_label):
+
+#     h2o.init()
+#     h2o_data = h2o.H2OFrame(train_data)
+#     h2o_y = h2o.H2OFrame(train_label)
+#     h2o_y = h2o_y.asfactor()
+#     df = h2o_data.cbind(h2o_y)
+#     train, valid = df.split_frame(seed = 1234, ratios=[0.8,0.2]
+#                              destination_frames=["train.hex", "test.hex"])
+#     xgb = H2OXGBoostEstimator(max_depth = 10 , ntrees = 200 , 
+#                           colsample_bytree = 0.9 , learn_rate= 0.01 , 
+#                             , col_sample_rate_per_tree = 0.9,
+#                           gamma = 0.0 , sample_rate = 0.9 , 
+#                           stopping_rounds = 50, stopping_tolerance = 0.001, 
+#                          )
+                         
+#     xgb.train(x= h2o_data.col_names ,
+#           y = h2o_y.col_names[0] ,
+#           training_frame= train ,
+#           validation_frame = valid ,
+#          )
+    X = train_data
+    y = train_label
+
+    xgb_model = xgb.XGBClassifier(objective="binary:logistic", random_state=42)
+    xgb_model.fit(X, y)
+
+    y_pred = xgb_model.predict(test_data)
+    
+    acc_test = accuracy_score(test_label, y_pred)
+    
+
+
+    with open('output/xgboost_baseline/testing_output.txt', 'w') as f:
+        f.truncate(0)
+
+    with open('output/xgboost_baseline/testing_output.txt', 'a') as f:
+        f.write("Accuracy: {}\n".format(acc_test))
+     
+        
+if __name__ == '__main__':
+    train_data = np.load('./data/train_data_baseline_1.npy')
+    test_data = np.load('./data/test_data_baseline_1.npy')
+    train_label = np.load('/scratch/bigdata/ABCD/abcd-fmriprep-rs/ST-GCN-data/harvardoxford/harvardoxford_train_label_1.npy')
+    test_label = np.load('/scratch/bigdata/ABCD/abcd-fmriprep-rs/ST-GCN-data/harvardoxford/harvardoxford_test_label_1.npy')
+    #train(train_data, test_data, train_label, test_label)
+    train_xgboost(train_data, test_data, train_label, test_label)
